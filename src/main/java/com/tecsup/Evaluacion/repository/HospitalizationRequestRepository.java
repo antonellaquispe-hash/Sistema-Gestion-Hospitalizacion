8d789a904a628ade1e1a489398ff7b73e9e41cfd
LeonardoRonda <leonardo.ronda@tecsup.edu.pe>
2026-09-10 21:50:32 -0500

    chore: agregar anotacion @Repository faltante en HospitalizationRequestRepository

--- src/main/java/com/tecsup/Evaluacion/repository/HospitalizationRequestRepository.java
+++ src/main/java/com/tecsup/Evaluacion/repository/HospitalizationRequestRepository.java
@@ -2,6 +2,8 @@ package com.tecsup.Evaluacion.repository;
 
 import com.tecsup.Evaluacion.model.HospitalizationRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface HospitalizationRequestRepository extends JpaRepository<HospitalizationRequest, Long> {
 }
